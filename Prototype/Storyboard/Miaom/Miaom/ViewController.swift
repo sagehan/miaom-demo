aec61a733dc89db14314b469bff9a2c874e8162f
Sage Han <zongshian@gmail.com>
2016-01-21 17:12:45 +0800

    -- Prototype -- ! A simple Xcode prototype

---
+++ Prototype/Storyboard/Miaom/Miaom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miaom
+//
+//  Created by ajmj on 16/1/21.
+//  Copyright © 2016年 Heysage.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
